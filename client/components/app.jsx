e7a0d13e26c24f82a5d2fc2a1a3327585f4c679e
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-23 19:26:27 +0000

    Prepped for new component render by adding user list component, deleting health checks, and giving App state.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -4,22 +4,15 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      view: { name: 'users' }
     };
   }
 
   componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
+
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+
   }
 }
