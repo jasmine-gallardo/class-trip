e0ace90903934e450f862817e36aefcea452434f
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-23 20:10:34 +0000

    Added setView method and imported UserList component to render.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserList from './user-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,11 +9,23 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-
+  setView() {
+    this.setState({
+      view: { name }
+    });
   }
 
   render() {
-
+    return (
+      <div>
+        <header className="bg-secondary d-flex justify-content-center">
+          <p className="m-auto text-light h4">APP NAME</p>
+        </header>
+        <div className="component-body p-4 pt-5">
+          <UserList setView={this.setView} />
+        </div>
+        <footer className="bg-secondary d-flex justify-content-between p-5"></footer>
+      </div>
+    );
   }
 }
