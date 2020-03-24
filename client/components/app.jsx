3c0395a11bf2297f438413e2f9f9ea6d847502ae
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-24 02:18:00 +0000

    Modified App to conditionally render different views/components. Imported Home component.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserList from './user-list';
+import Home from './home';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,13 +20,22 @@ export default class App extends React.Component {
   }
 
   render() {
+    let view;
+    switch (this.state.view.name) {
+      case 'users': view =
+        <UserList setView={this.setView} />;
+        break;
+      case 'loggedIn': view =
+        <Home setView={this.setView} userName={this.state.user.userName}/>;
+        break;
+    }
     return (
       <div>
         <header className="bg-secondary d-flex justify-content-center">
           <p className="m-auto text-light h4">APP NAME</p>
         </header>
         <div className="component-body p-4 pt-5">
-          <UserList setView={this.setView} />
+          {view}
         </div>
         <footer className="bg-secondary d-flex justify-content-between p-5"></footer>
       </div>
