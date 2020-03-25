c0412572dbca5ded8dec2fe1f899551532d0cd78
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-25 22:15:58 +0000

    Created POST method to /api/field_trips so user can add a field trip.

--- server/index.js
+++ server/index.js
@@ -261,6 +261,85 @@ where   "categoryName" = $1
     .catch(err => next(err));
 });
 
+app.post('/api/field_trips', (req, res, next) => {
+  const fieldTripName = req.body.name;
+  const description = req.body.description;
+  const address = req.body.address;
+  const city = req.body.city;
+  const date = req.body.date;
+  const time = req.body.time;
+  const categoryId = req.body.categoryId;
+  const userId = req.body.userId;
+
+  if (!fieldTripName || !description || !address || !city || !date || !time || !categoryId || !userId) {
+    return (
+      res.status(400).json({ error: 'All field trip details are required.' })
+    );
+  }
+
+  const sql = `
+    insert into "field_trips" ("fieldTripName", "description", "address", "city", "date", "time")
+    values ($1, $2, $3, $4, $5, $6)
+    returning "fieldTripId"
+  `;
+  const values = [fieldTripName, description, address, city, date, time];
+  db.query(sql, values)
+    .then(result => {
+      const fieldTripId = result.rows[0].fieldTripId;
+      const sql = `
+        insert into "field_trips_categories" ("fieldTripId", "categoryId")
+        values ($1, $2)
+        returning "fieldTripId"
+      `;
+      const values = [fieldTripId, categoryId];
+      return (
+        db.query(sql, values)
+          .then(result => {
+            const fieldTripId = result.rows[0].fieldTripId;
+            return fieldTripId;
+          })
+      );
+    })
+    .then(result => {
+      const fieldTripId = result;
+      const userId = req.body.userId;
+      const sql = `
+        insert into "users_field_trips" ("fieldTripId", "userId")
+        values ($1, $2)
+        returning *
+      `;
+      const values = [fieldTripId, userId];
+      return (
+        db.query(sql, values)
+          .then(result => {
+            return (
+              result.rows[0]
+            );
+          })
+      );
+    })
+    .then(result => {
+      const fieldTripId = result.fieldTripId;
+      const sql = `
+        select "ft"."fieldTripId",
+              "ft"."fieldTripName",
+              "ft"."description",
+              "ft"."address",
+              "ft"."city",
+              "ft"."date",
+              "ft"."time",
+              "uft"."userId"
+          from "field_trips" as "ft"
+          join "users_field_trips" as "uft" using ("fieldTripId")
+          where "ft"."fieldTripId" = $1
+      `;
+      const values = [fieldTripId];
+      db.query(sql, values)
+        .then(result => res.status(201).json(result.rows[0]));
+    })
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
