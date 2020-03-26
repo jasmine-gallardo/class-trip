dd438db47e0e89a8df9918553a81d8eb8cf21236
Arlette Pineda <arlettepimo@gmail.com>
2020-03-26 19:29:23 +0000

    Can now click through from User's home when logged in and can click Courses button to go to Search Courses page.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import UserList from './user-list';
 import Home from './home';
 import UserCourses from './user-courses';
-import SearchCourses from './search-courses'
+import SearchCourses from './search-courses';
 import UserLessons from './user-lessons';
-
 
 export default class App extends React.Component {
   constructor(props) {
@@ -34,6 +33,9 @@ export default class App extends React.Component {
       case 'loggedIn': view =
         <Home setView={this.setView} userName={this.state.user.userName} userId={this.state.user.userId} />;
         break;
+      case 'searchCourses': view =
+        <SearchCourses setView={this.setView} userName={this.state.user.userName} />;
+        break;
       case 'myCourses': view =
         <UserCourses setView={this.setView} userName={this.state.user.userName} userId={this.state.user.userId} />;
         break;
@@ -46,8 +48,7 @@ export default class App extends React.Component {
           <p className="m-auto text-light h4">APP NAME</p>
         </header>
         <div className="component-body p-4 pt-5">
-          {/* {view} */}
-          <SearchCourses />
+          {view}
         </div>
         <footer className="bg-secondary d-flex justify-content-between p-5"></footer>
       </div>
--- client/components/search-courses.jsx
+++ client/components/search-courses.jsx
@@ -6,26 +6,120 @@ export default class SearchCourses extends React.Component {
     this.state = {
       categories: [],
       courses: []
-    }
+    };
   }
+
+componentDidMount() {
+  this.getCategories();
+}
+
+getCategories() {
+    fetch('/api/categories')
+      .then(res => res.json())
+      .then(categoriesArray => this.setState({ categories: categoriesArray }))
+      .catch(err => console.error(err));
+}
+
+getCatCourses(courseName) {
+    fetch('/api/courses')
+      .then(res => res.json())
+      .then(coursesArray => this.setState({ courses: coursesArray }))
+      .catch(err => console.error(err));
+}
+
 
   render() {
     return (
-      <div className="component-body p-4 pt-5">
+      <div>
         <div className="mt-4 ml-3 h-75" id="for-cat-search" >
           <label htmlFor="search-cat">Search by Category:</label>
           <input className="search-box" list="categories" id="cat-search" name="category-search" />
           <datalist id="categories">
             <option value="Economics">Economics</option>
-            <option value="Engineering">Engineering</option>
-            <option value="English">English</option>
-            <option value="Film">Film</option>
-            <option value="Art">Art</option>
-            <option value="Finance">Finance</option>
           </datalist>
         </div>
+      </div >
+    );
+  }
+  }
 
-        <div id="popular-courses d-flex row">
+// function Result(props) {
+//   return (
+//     // map through courses here
+//     // add result box with button
+//   )
+// }
+
+
+//
+// import React from 'react';
+
+// export default class UserCourses extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       courses: []
+//     };
+//     this.getCourses = this.getCourses.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.getCourses(this.props.userId);
+//   }
+
+//   getCourses(userId) {
+//     fetch(`/api/users_courses/${userId}`)
+//       .then(res => res.json())
+//       .then(coursesArray => this.setState({ courses: coursesArray }))
+//       .catch(err => console.error(err));
+//   }
+
+//   render() {
+//     return (
+//       <div className="h-75 d-flex flex-wrap justify-content-center">
+//         <div className="w-50 m-auto p-2 d-flex justify-content-center justify-content-between">
+//           <i className="far fa-bookmark fa-3x"></i>
+//           <p className="h2">Courses</p>
+//         </div>
+//         {
+//           this.state.courses.map(course => {
+//             return (
+//               <Course
+//                 key={course.courseId}
+//                 name={course.name}
+//                 userName={this.props.userName}
+//                 userId={this.props.userId}
+//                 courseId={course.courseId}
+//                 setView={this.props.setView}
+//               />
+//             );
+//           })
+//         }
+//       </div>
+//     );
+//   }
+// }
+
+// function Course(props) {
+//   const userName = props.userName;
+//   const userId = props.userId;
+//   const courseId = props.courseId;
+//   const courseName = props.name;
+
+//   return (
+//     <div className="w-100 d-flex justify-content-center">
+//       <button
+//         onClick={() => props.setView('myLessons', userName, userId, courseId)}
+//         className="w-100 btn-block btn-warning text-light h4 mb-3 rounded">
+//         {courseName}
+//       </button>
+//     </div>
+//   );
+// }
+
+
+          {/* THIS IS POPULAR COURSES SECTION */}
+        {/* <div id="popular-courses d-flex row">
           <h6 className="text-center">Popular Courses</h6>
           <div className="d-flex justify-content-around">
             <div className="pop-course d-inline-flex justify-content-center align-items-center" id="painting">
@@ -39,8 +133,4 @@ export default class SearchCourses extends React.Component {
               </div>
             </div>
           </div>
-        </div>
-      </div >
-    );
-  }
-}
+        </div> */}
