728f3e9de5a82c1491f1e4043ac408471e61d039
melissamurphy <flylittlefeather@yahoo.com>
2020-03-27 22:09:57 +0000

    completed non-conditional rendering for Header

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -4,21 +4,23 @@ import Home from './home';
 import UserCourses from './user-courses';
 import UserLessons from './user-lessons';
 import UserFieldTrips from './user-field-trips';
-import Header from './header';
-import Navbar from './navbar';
 import FieldTripForm from './field-trip-form';
+import Header from './header';
+// import Navbar from './navbar';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       view: { name: 'users' },
+      headerView: {name: 'appLogo'},
       user: { userName: '', userId: null },
       course: { courseId: null },
       fieldTrip: { fieldTripId: null },
       allFieldTrips: []
     };
     this.setView = this.setView.bind(this);
+    this.setHeaderView = this.setHeaderView.bind(this);
     this.addFieldTrip = this.addFieldTrip.bind(this);
   }
 
@@ -46,23 +48,38 @@ export default class App extends React.Component {
     });
   }
 
+  setHeaderView(name) {
+    this.setState({
+      headerView: { name }
+    })
+  }
+
   render() {
     let view;
+    let headerView;
     switch (this.state.view.name) {
       case 'users': view =
         <UserList setView={this.setView} />;
+        headerView =
+        <Header view={'users'} />
         break;
       case 'loggedIn': view =
         <Home setView={this.setView} userName={this.state.user.userName} userId={this.state.user.userId} />;
+        headerView =
+        <Header view={'loggedIn'} />
         break;
       case 'myCourses': view =
         <UserCourses setView={this.setView} userName={this.state.user.userName} userId={this.state.user.userId} />;
+        headerView =
+        <Header view={'myCourses'}/>
         break;
       case 'myLessons': view =
         <UserLessons setView={this.setView} courseId={this.state.course.courseId} userId={this.state.user.userId}/>;
         break;
       case 'myFieldTrips': view =
         <UserFieldTrips setView={this.setView} userName={this.state.user.userName} userId={this.state.user.userId} courseId={this.state.course.courseId}/>;
+        headerView =
+        <Header view={'myFieldTrips'} />
         break;
       case 'planFieldTrip': view =
         <FieldTripForm setView={this.setView} addFieldTrip={this.addFieldTrip} user={this.state.user}/>;
@@ -72,12 +89,12 @@ export default class App extends React.Component {
         {/* <header className="bg-secondary d-flex justify-content-center">
           <p className="m-auto text-light h4">APP NAME</p>
         </header> */}
-        <Header setView={this.setView} />
+        {/* <Header setView={this.setView} /> */}
+        {headerView}
         <div className="component-body p-4 pt-5">
           {view}
         </div>
-        {/* <footer className="bg-secondary d-flex justify-content-between p-5"></footer> */}
-        <Navbar />
+        <footer className="bg-secondary d-flex justify-content-between p-5"></footer>
       </div>
     );
   }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,13 +5,37 @@ export default class Header extends React.Component {
     super(props);
   }
 
-  render() {
-    const currentPage = this.props.currentPage;
-    const setView = this.props.setView;
-    return (
-      <header className="bg-secondary d-flex justify-content-center">
+  getHeaderContent() {
+    switch (this.props.view) {
+      case 'users':
+      case 'loggedIn':
+        console.log('logged in')
+        return (
         <p className="m-auto text-light h4"><i className="fas fa-globe-americas"></i>
           <i className="fas fa-copyright ml-n1"></i><span className="mb-2">lassroom</span></p>
+      )
+      break;
+      case 'myCourses':
+        console.log('myCourses');
+        return (
+          <h2>My Courses</h2>
+          )
+      break;
+      case 'myFieldTrips':
+        console.log('myFieldTrips');
+        return (
+          <h2>My Field Trips</h2>
+        )
+    }
+  }
+
+  render() {
+    const setHeaderView = this.props.setHeaderView;
+    const view = this.props.view;
+    const userName = this.props.userName;
+    return (
+      <header className="bg-secondary d-flex justify-content-center align-items-center text-light">
+        {this.getHeaderContent()}
       </header>
     );
   }
