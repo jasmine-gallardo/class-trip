d5312bbb71a1ccc0b06ef57d7ec31084045f6586
Arlette Pineda <arlettepimo@gmail.com>
2020-03-29 03:56:58 +0000

    Added initial EditFieldTrip class component.

---
+++ client/components/edit-field-trip.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class EditFieldTrip extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return ('hello')
+  }
+}
