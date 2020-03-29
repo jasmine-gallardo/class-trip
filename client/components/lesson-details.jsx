81996b22400677c14e5b5c16e1af5f1a9cebcfd6
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-29 01:30:57 +0000

    Added custom font and bootstrap style to lesson details.

--- client/components/lesson-details.jsx
+++ client/components/lesson-details.jsx
@@ -21,9 +21,9 @@ export default class LessonDetails extends React.Component {
 
   render() {
     return (
-      <div className="bg-light h-100 p-2">
-        <h2 className="mb-5">{this.state.lesson.heading}</h2>
-        <div className="mb-5">
+      <div className="h-100 p-2">
+        <h2 className="lesson-details-header mb-5 text-info text-center display-4">{this.state.lesson.heading}</h2>
+        <div className="mb-5 lead text-secondary">
           {this.state.lesson.body}
         </div>
       </div>
