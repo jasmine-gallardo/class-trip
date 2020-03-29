400a299cea9df000e170916699acab5b27df20a0
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-29 05:51:36 +0000

    Adjusted navbar icon color and size.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -4,14 +4,14 @@ export default class Navbar extends React.Component {
 
   render() {
     return (
-      <footer className="bg-secondary d-flex align-items-center justify-content-between p-4">
+      <footer className="bg-secondary d-flex align-items-center justify-content-between p-3 text-light">
         <div onClick={() => this.props.setView('loggedIn')}>
           <i
-            className="fas fa-home fa-2x"></i>
+            className="fas fa-home fa-3x"></i>
         </div>
         <div onClick={() => this.props.setView('planFieldTrip')}>
           <i
-            className="far fa-clipboard fa-2x"></i>
+            className="far fa-clipboard fa-3x"></i>
         </div>
       </footer>
 
