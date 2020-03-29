95c0517c0126c94f04480745b600e1e42da3e3ef
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-29 18:36:25 +0000

    Re-styled list of courses.

--- client/components/course.jsx
+++ client/components/course.jsx
@@ -17,12 +17,19 @@ export default class Course extends React.Component {
     const courseId = this.props.courseId;
     const backPage = this.props.currentPage;
     return (
-      <div className="w-100 d-flex justify-content-center">
-        <button
-          onClick={() => this.setNextPage('myLessons', courseId, backPage)}
-          className="w-100 btn-block btn-info shadow text-light h4 mb-3 rounded">
-          {courseName}
-        </button>
+      <div className="col-12 card p-3 mb-1 bg-light">
+        <div className="row my-auto">
+          <div className="col-9 py-3 pl-5">
+            <div className="h4 open-sans mb-0">
+              {courseName}
+            </div>
+          </div>
+          <div className="d-flex flex-wrap align-items-center col-3">
+            <button
+              onClick={() => this.setNextPage('myLessons', courseId, backPage)}
+              type="button" className="btn btn-info my-1 ">&gt;</button>
+          </div>
+        </div>
       </div>
     );
   }
