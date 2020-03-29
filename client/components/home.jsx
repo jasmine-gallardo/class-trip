a78ce0a8d49e195faf4ed50ff8c18d2915b659eb
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-29 06:54:05 +0000

    Modified Home page style.

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -9,29 +9,31 @@ export default class Home extends React.Component {
   render() {
     const userName = this.props.userName;
     return (
-      <div>
-        <div className="w-50 m-auto p-2 d-flex justify-content-center justify-content-between">
-          <i className="fas fa-user fa-3x"></i>
-          <p className="h2 ml-2">{userName}</p>
+      <div className="h-100">
+        <div className="w-100 m-auto p-2 d-flex justify-content-center">
+          {/* <i className="fas fa-user fa-3x text-info"></i> */}
+          <p className="display-4 ml-2 mb-4 text-info">{userName}</p>
         </div>
-        <div className="search-buttons mt-5 mb-5 d-flex justify-content-center align-items-center">
+        <p className="w-75 h4 m-auto ">Search</p>
+        <div className=" general h-25 mt-3 text-center">
           <button
             onClick={() => this.props.setView('searchCourses')}
-            className="courses pt-5 pb-5 pr-4 pl-4 mr-1 text-light h4" type="submit">Courses</button>
+            className="w-75 btn-lg btn-light shadow m-2" type="submit">Courses</button>
           <button
             onClick={() => this.props.setView('fieldTrips')}
-            className="field-trips pt-5 pb-5 pr-3 pl-3 ml-1 text-light h4 ">Field Trips</button>
+            className="w-75 btn-lg btn-light shadow m-2">Field Trips</button>
         </div>
-        <div className="text-center">
+        <p className="w-75 h4 m-auto ">Me</p>
+        <div className="h-50 mt-3 text-center">
           <div>
             <button
               onClick={() => this.props.setView('myCourses')}
-              className="w-75 btn-lg btn-dark m-2" type="submit">My Courses</button>
+              className="w-75 btn-lg btn-light shadow m-2" type="submit">My Courses</button>
           </div>
           <div>
             <button
               onClick={() => this.props.setView('myFieldTrips')}
-              className="w-75 btn-lg btn-dark m-2" type="submit">My Field Trips</button>
+              className="w-75 btn-lg btn-light shadow m-2" type="submit">My Field Trips</button>
           </div>
         </div>
       </div>
