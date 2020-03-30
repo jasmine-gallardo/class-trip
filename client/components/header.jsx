24ca3b406148b0257c2c1ac1a7d36340f7a34ffe
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-30 00:56:13 +0000

    Fixed header spacing.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,55 +7,54 @@ export default class Header extends React.Component {
       case 'users':
       case 'loggedIn':
         return (
-          <p className="m-auto text-light h4"><i className="fas fa-globe-americas"></i>
-            <i className="fas fa-copyright ml-n1"></i><span className="mb-2">lassroom</span></p>
+          <p className="work-sans h2">ClassTrip</p>
         );
       case 'searchCourses':
         return (
           <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
-            <i className="fas fa-search fa-3x mr-3"></i>
-            <p className="h2"> Search for Courses</p>
+            <i className="fas fa-search fa-2x mr-3"></i>
+            <p className="h4 work-sans mb-0"> Search for Courses</p>
           </div>
         );
       case 'myCourses':
         return (
           <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
-            <i className="far fa-bookmark fa-3x mr-3"></i>
-            <p className="h2"> My Courses</p>
+            <i className="far fa-bookmark fa-2x mr-3"></i>
+            <p className="h4 work-sans mb-0"> My Courses</p>
           </div>
         );
       case 'myFieldTrips':
         return (
           <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
-            <i className="far fa-calendar-alt fa-3x mr-3"></i>
-            <p className="h2"> My Field Trips</p>
+            <i className="far fa-calendar-alt fa-2x mr-3"></i>
+            <p className="h4 work-sans mb-0"> My Field Trips</p>
           </div>
         );
       case 'planFieldTrip':
         return (
           <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
-            <i className="far fa-clipboard fa-3x mr-3"></i>
-            <p className="h2"> Plan Field Trip</p>
+            <i className="far fa-clipboard fa-2x mr-3"></i>
+            <p className="h4 work-sans mb-0"> Plan Field Trip</p>
           </div>
         );
       case 'myLessons':
         return (
-          <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
+          <div className="w-100 m-auto p-2 d-flex justify-content-center justify-content-between row">
             <div onClick={() => this.props.setView(this.props.backPage)}>
               <i
-                className="fas fa-angle-left fa-3x mr-3"></i>
+                className="fas fa-angle-left fa-3x m-auto pl-3 col-2"></i>
             </div>
-            <p className="h2"> Lessons</p>
+            <p className="h4 work-sans m-auto col-6"> Lessons</p>
           </div>
         );
       case 'lessonDetails':
         return (
-          <div className="w-70 m-auto p-2 d-flex justify-content-center justify-content-between">
+          <div className="w-100 row m-auto p-2 d-flex justify-content-center justify-content-between">
             <div onClick={() => this.props.setView('myLessons')}>
               <i
-                className="fas fa-angle-left fa-3x mr-3"></i>
+                className="fas fa-angle-left fa-3x m-auto pl-3 col-2"></i>
             </div>
-            <p className="h2"> Details</p>
+            <p className="h4 work-sans m-auto col-6"> Details</p>
           </div>
         );
     }
