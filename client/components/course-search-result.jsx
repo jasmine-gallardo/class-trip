a094ca8f9b8ff1a78280da8772af9d37525e9339
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-30 00:31:31 +0000

    Cleaned up onClick syntax.

--- client/components/course-search-result.jsx
+++ client/components/course-search-result.jsx
@@ -18,7 +18,7 @@ export default class CourseSearchResult extends React.Component {
     const courseDesc = this.props.courseDesc;
     const backPage = this.props.currentPage;
     return (
-      <div className="col-12 card p-3 mb-1 bg-light">
+      <div className="col-12 card p-3 mb-1 bg-lightm mb-2">
         <div className="row">
           <div className="col-9">
             <div className="h4">
@@ -30,7 +30,7 @@ export default class CourseSearchResult extends React.Component {
           </div>
           <div className="d-flex flex-wrap align-items-center">
             <button
-              onClick={() => { this.setNextPage('myLessons', courseId, backPage); }}
+              onClick={() => this.setNextPage('myLessons', courseId, backPage)}
               type="button" className="btn btn-dark my-1 "> INFO </button>
           </div>
         </div>
