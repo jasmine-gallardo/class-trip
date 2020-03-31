3c36eacb97a36a12aa15432694ddd5caef2140e2
Arlette Pineda <arlettepimo@gmail.com>
2020-03-31 06:29:34 +0000

    Changed hard coded values used during test.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -15,10 +15,10 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: { name: 'editFieldTrip' },
-      user: { userName: 'Melissa', userId: 1 },
+      view: { name: 'users' },
+      user: { userName: '', userId: null },
       course: { courseId: null },
-      fieldTrip: { fieldTripId: 1 },
+      fieldTrip: { fieldTripId: null },
       allFieldTrips: [],
       lessons: [],
       lessonId: null,
