1ca7d7fe5ddd14ee0964e8b1fc78b3812eb8bf06
Melissa Murphy <flylittlefeather@yahoo.com>
2020-03-31 00:58:11 +0000

    Remade into static navbar that highlights icon current page

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -2,36 +2,32 @@ import React from 'react';
 
 export default class Navbar extends React.Component {
   getHomeClass() {
-    if (this.props.view === 'users' || this.props.view === 'loggedIn') {
-      return 'hidden';
+    if (this.props.view !== 'loggedIn') {
+      return 'text-dark';
     }
   }
 
   getSearchClass() {
-    if (this.props.view === 'generalSearch' || this.props.view === 'users' || this.props.view === 'myLessons' || this.props.view === 'lessonDetails') {
-      return 'd-none';
+    if (this.props.view !== 'generalSearch' && this.props.view !== 'searchCourses') {
+      return 'text-dark';
     }
   }
 
   getPlanClass() {
-    if (this.props.view === 'planFieldTrip' || this.props.view === 'users') {
-      return 'd-none';
+    if (this.props.view !== 'planFieldTrip') {
+      return 'text-dark';
     }
   }
 
   getLogoutClass() {
-    if (this.props.view !== 'loggedIn') {
-      return 'd-none';
+    if (this.props.view !== 'users') {
+      return 'text-dark';
     }
   }
 
   render() {
     return (
       <footer className="bg-secondary d-flex align-items-center justify-content-between p-3 text-light">
-        <div className={this.getLogoutClass()} onClick={() => this.props.setView('users')}>
-          <i
-            className="fas fa-users-cog fa-3x"></i>
-        </div>
         <div className={this.getHomeClass()} onClick={() => this.props.setView('loggedIn')}>
           <i
             className="fas fa-home fa-3x"></i>
@@ -43,6 +39,10 @@ export default class Navbar extends React.Component {
         <div className={this.getPlanClass()} onClick={() => this.props.setView('planFieldTrip')}>
           <i
             className="far fa-clipboard fa-3x"></i>
+        </div>
+        <div className={this.getLogoutClass()} onClick={() => this.props.setView('users')}>
+          <i
+            className="fas fa-users-cog fa-3x"></i>
         </div>
       </footer>
 
