5d7a5d6c84661da896e21fdf2833dc2e82222f9f
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-31 22:41:56 +0000

    Re-styled view field trip details.

--- client/components/field-trips-details.jsx
+++ client/components/field-trips-details.jsx
@@ -21,30 +21,27 @@ export default class FieldTripsDetails extends React.Component {
   render() {
     const fieldTrip = this.state.fieldTripDetails;
     return (
-      <div>
-        <div className="open-sans h2 text-info text-center">{fieldTrip.fieldTripName}</div>
-        <div className='row'>
-          <h4 className="text-secondary open-sans m-auto text-center">{fieldTrip.date}</h4>
-        </div>
+      <div className="h-100">
         <div>
-          <p className="lead mt-1 mb-4 text-center">{fieldTrip.time}</p>
+          <p className="text-secondary lead mt-3">{fieldTrip.date} @ {fieldTrip.time}</p>
         </div>
+        <div className="open-sans h3 text-info">{fieldTrip.fieldTripName}</div>
         <div className='row p-3 h4 my-3'>
-          <i className="fa fa-map-marker mr-3"></i>
-          <div>
+          <i className="fas fa-map-marker-alt ml-2 mr-3 mt-2"></i>
+          <div className="align-items-center">
             <p className="lead m-0">{fieldTrip.address}</p>
             <p className="lead m-0">{fieldTrip.city}</p>
           </div>
         </div>
         <div className='row p-3'>
-          <div className='col-md-6 text-center'>
+          <div className='col-md-6 text-center mb-4'>
             <div id="map-container-google-1" className="z-depth-1-half map-container bg-light">
             </div>
           </div>
         </div>
         <div className='row'>
-          <div className='col-md-8'>
-            <h4>Details:</h4>
+          <div className='col-md-8 align-middle'>
+            <p className="h4">Details:</p>
           </div>
         </div>
         <div className='col-md-8 lead'>
