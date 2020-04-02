5efdb0a8b1c214663dd18d471e09558e3404cc99
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-04-02 02:06:55 +0000

    Added setViewandFieldTripId method and adjusted style.

--- client/components/field-trips-details.jsx
+++ client/components/field-trips-details.jsx
@@ -5,6 +5,7 @@ export default class FieldTripsDetails extends React.Component {
     super(props);
     this.state = { fieldTripDetails: {} };
     this.getFieldTripDetails = this.getFieldTripDetails.bind(this);
+    this.setViewAndFieldTripId = this.setViewAndFieldTripId.bind(this);
   }
 
   componentDidMount() {
@@ -18,11 +19,17 @@ export default class FieldTripsDetails extends React.Component {
       .catch(err => console.error(err));
   }
 
+  setViewAndFieldTripId(viewName, fieldTripId) {
+    this.props.setView(viewName);
+    this.props.setFieldTrip(fieldTripId);
+  }
+
   render() {
+    const fieldTripId = this.props.fieldTripId;
     const fieldTrip = this.state.fieldTripDetails;
     return (
       <div className="h-100">
-        <div className="">
+        <div>
           <div>
             <p className="text-secondary lead mt-3">{fieldTrip.date} @ {fieldTrip.time}</p>
           </div>
@@ -35,17 +42,21 @@ export default class FieldTripsDetails extends React.Component {
             <p className="lead m-0">{fieldTrip.city}</p>
           </div>
         </div>
-        <div className="h-50 card p-3 mb-5">
-          <div className='row'>
-            <div className='col-md-8 align-middle'>
-              <p className="h4">Details:</p>
+        <div>
+          <div className="h-50 card p-3 mb-5">
+            <div className='row'>
+              <div className='col-md-8 align-middle'>
+                <p className="h4">Details:</p>
+              </div>
+            </div>
+            <div className='col-md-8 lead'>
+              <p>{fieldTrip.description}</p>
             </div>
           </div>
-          <div className='col-md-8 lead'>
-            <p>{fieldTrip.description}</p>
-          </div>
         </div>
-        <div className="text-center align-middle">
+        <div
+          onClick={() => this.setViewAndFieldTripId('editFieldTrip', fieldTripId)}
+          className="text-center align-middle">
           <i className="fas fa-edit h3"></i>
         </div>
       </div>
