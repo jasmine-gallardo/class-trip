9f9409e0b85fc3dfbf908246bad76dc9bfe421f7
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-04-02 01:01:36 +0000

    Added edit icon.

--- client/components/field-trips-details.jsx
+++ client/components/field-trips-details.jsx
@@ -22,30 +22,31 @@ export default class FieldTripsDetails extends React.Component {
     const fieldTrip = this.state.fieldTripDetails;
     return (
       <div className="h-100">
-        <div>
-          <p className="text-secondary lead mt-3">{fieldTrip.date} @ {fieldTrip.time}</p>
+        <div className="">
+          <div>
+            <p className="text-secondary lead mt-3">{fieldTrip.date} @ {fieldTrip.time}</p>
+          </div>
+          <div className="open-sans h3 text-info">{fieldTrip.fieldTripName}</div>
         </div>
-        <div className="open-sans h3 text-info">{fieldTrip.fieldTripName}</div>
-        <div className='row p-3 h4 my-3'>
+        <div className='row p-3 h4 my-3 mb-2'>
           <i className="fas fa-map-marker-alt ml-2 mr-3 mt-2"></i>
           <div className="align-items-center">
             <p className="lead m-0">{fieldTrip.address}</p>
             <p className="lead m-0">{fieldTrip.city}</p>
           </div>
         </div>
-        <div className='row p-3'>
-          <div className='col-md-6 text-center mb-4'>
-            <div id="map-container-google-1" className="z-depth-1-half map-container bg-light">
+        <div className="h-50 card p-3 mb-5">
+          <div className='row'>
+            <div className='col-md-8 align-middle'>
+              <p className="h4">Details:</p>
             </div>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-md-8 align-middle'>
-            <p className="h4">Details:</p>
+          <div className='col-md-8 lead'>
+            <p>{fieldTrip.description}</p>
           </div>
         </div>
-        <div className='col-md-8 lead'>
-          <p>{fieldTrip.description}</p>
+        <div className="text-center align-middle">
+          <i className="fas fa-edit h3"></i>
         </div>
       </div>
     );
